396fde45dd6006160d6f79e33dd567d77d927a30
tawatr <tawat.r2d2@gmail.com>
2017-09-10 10:29:33 +0700

    UI done

--- numberfunny/ViewController.swift
+++ numberfunny/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var numberString="";
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+
+    @IBAction func decreaseAction(_ sender: Any) {
+        var number1=Int(numberLabel.text!)
+        print( "Decrease button clicked" )
+        print( "Input number \(String(describing: numberLabel.text))")
+        numberLabel.text=String(number1!-1)
+    }
+    
+    @IBAction func increaseAction(_ sender: Any) {
+        var number1=Int(numberLabel.text!)
+        print( "Increase button clicked" )
+        print( "Input number \(String(describing: numberLabel.text))")
+        numberLabel.text=String(number1!+1)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
