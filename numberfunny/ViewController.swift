f4f6c47bb9b5ed15faaae3f8a6add0d3f3bfe3e9
tawatr <tawat.r2d2@gmail.com>
2017-09-10 13:46:21 +0700

    Add second page

--- numberfunny/ViewController.swift
+++ numberfunny/ViewController.swift
@@ -15,23 +15,41 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var numberLabel: UILabel!
     
+    func addInt( n1Int: Int ) -> Int{
+        let result = n1Int+1;
+        return result;
+    }
+    func decInt( n1Int: Int ) -> Int{
+        let result = n1Int-1;
+        return result;
+    }
     
 
     @IBAction func decreaseAction(_ sender: Any) {
         var number1=Int(numberLabel.text!)
         print( "Decrease button clicked" )
         print( "Input number \(String(describing: numberLabel.text))")
-        numberLabel.text=String(number1!-1)
+        //        numberLabel.text=String(number1!-1)
+//        numberLabel.text=String(decInt( n1Int: number1! ))
+        var cls1 = testclass1();
+        numberLabel.text=String( cls1.decNumber(n1Int: number1!) );
     }
     
     @IBAction func increaseAction(_ sender: Any) {
         var number1=Int(numberLabel.text!)
         print( "Increase button clicked" )
         print( "Input number \(String(describing: numberLabel.text))")
-        numberLabel.text=String(number1!+1)
+//        numberLabel.text=String(number1!+1)
+        numberLabel.text=String(addInt( n1Int: number1! ))
     }
     
+    func Zeroing( _: Void ) -> Int{
+        return 0;
+    }
     
+    @IBAction func resetAction(_ sender: Any) {
+        numberLabel.text=String( Zeroing() )
+    }
     
     
     override func viewDidLoad() {
---
+++ numberfunny/testclass1.swift
@@ -0,0 +1,15 @@
+//
+//  testclass1.swift
+//  numberfunny
+//
+//  Created by Tawat Rung-Arunwan on 9/10/17.
+//  Copyright © 2017 TAWATER. All rights reserved.
+//
+
+import Foundation
+
+class testclass1{
+    func decNumber( n1Int: Int) -> Int{
+        return n1Int-1;
+    }
+}
