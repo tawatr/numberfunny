04374acf20d044464743bb3003ab768697d27b4b
tawatr <tawat.r2d2@gmail.com>
2017-09-10 14:25:28 +0700

    2nd page done

--- numberfunny/TimerTextViewController.swift
+++ numberfunny/TimerTextViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TimerViewController.swift
+//  TimerTextViewController.swift
 //  numberfunny
 //
 //  Created by Tawat Rung-Arunwan on 9/10/17.
@@ -8,29 +8,8 @@
 
 import UIKit
 
-class TimerViewController: UIViewController {
-    
-    
-    var numberAInt: Int = 1
-    var objTimer = Timer()
-    
-    func playTime() -> Void {
-        numberAInt += 1
-        numberLabel.text=String( numberAInt )
-    }
-    
-    @IBAction func playButton(_ sender: Any) {
-        print("Play button clicked")
-        objTimer=Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.playTime), userInfo: nil, repeats: true)
-    }
-    
-    @IBAction func stopButton(_ sender: Any) {
-        print("Stop button clicked")
-        objTimer.invalidate()
-    }
-    
-    @IBOutlet weak var numberLabel: UILabel!
-    
+class TimerTextViewController: UIViewController {
+
     
     
     
