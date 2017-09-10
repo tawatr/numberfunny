c26600545ae67f129fcb4a5320f9459be2f262a9
tawatr <tawat.r2d2@gmail.com>
2017-09-10 09:30:06 +0700

    Initial Commit

---
+++ numberfunny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  numberfunny
+//
+//  Created by Tawat Rung-Arunwan on 9/10/17.
+//  Copyright © 2017 TAWATER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
